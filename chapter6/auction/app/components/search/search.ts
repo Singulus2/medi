5834fa5409b6ad939e7a6aa4098be558a94e2a9e
Singulus2 <bernd.kirchner@devk.de>
2016-04-27 11:58:58 +0200

    Delete chapter 1-9
    Delete chapter 1-9

--- chapter6/auction/app/components/search/search.ts
+++
@@ -1,7 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-  selector: 'auction-search',
-  templateUrl: 'app/components/search/search.html'
-})
-export default class SearchComponent {}
