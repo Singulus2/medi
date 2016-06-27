0c454b2d542a2b534ca3363e69afe573d444d9a4
Bernd Kirchner <bernd.kirchner@googlemail.com>
2016-06-27 12:26:17 +0200

    Update vendor.ts

--- chapter10/auction/client/src/vendor.ts
+++ chapter10/auction/client/src/vendor.ts
@@ -15,3 +15,16 @@ import 'rxjs';
 import 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+var firebase = require("firebase/app");
+  //require("firebase/auth");
+  require("firebase/database");
+  require("firebase/storage");
+
+  var config = {
+    apiKey: "AIzaSyAsLRL-2dimMxBp6_DYd7Q-5xy8kU_JVKw",
+    authDomain: "shining-inferno-5332.firebaseapp.com",
+    databaseURL: "https://shining-inferno-5332.firebaseio.com",
+    storageBucket: "shining-inferno-5332.appspot.com",
+  };
+  firebase.initializeApp(config);
